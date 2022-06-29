05f6103afce07b71a9dad069e4ec7d0ab959abe0
[Andy Gielis] <[andy.gielis@telenet.be]>
2022-06-29 08:42:10 +0200

    Wpboilerplate

---
+++ boilerplatewp/404.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_template_part( 'partials/header' ); ?>
+
+
+
+<?php get_template_part( 'partials/footer' ); ?>
---
+++ boilerplatewp/partials/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width">
+    <?php wp_head(); ?>
+    <link rel="stylesheet" href="https://use.typekit.net/mzi5pwi.css">
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.0.0/css/all.css">
+</head>
+
+<body <?php body_class(); ?>>
+
+    
